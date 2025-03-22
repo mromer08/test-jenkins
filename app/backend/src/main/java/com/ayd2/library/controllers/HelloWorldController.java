ac8ad21254abc7d111d959d9e6f9851b133a0831
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-03-21 23:13:13 -0600

    envfile

--- app/backend/src/main/java/com/ayd2/library/controllers/HelloWorldController.java
+++ app/backend/src/main/java/com/ayd2/library/controllers/HelloWorldController.java
@@ -1,17 +1,14 @@
 package com.ayd2.library.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController("/ping")
 public class HelloWorldController {
 
-    @Value("${super.secret}")
-    String secret;
     @GetMapping("/ping")
     public String ping() {
-        return "pong"+secret;
+        return "pong";
     }   
 }
