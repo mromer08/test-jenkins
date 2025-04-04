1faa4f2af124959143416e302901a3857e6412a5
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-04-04 01:58:19 -0600

    test hoo

--- app/backend/src/main/java/com/ayd2/library/controllers/author/AuthorController.java
+++ app/backend/src/main/java/com/ayd2/library/controllers/author/AuthorController.java
@@ -38,7 +38,7 @@ public class AuthorController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteAuthor(@PathVariable UUID id) {
         if (authorService.getAuthorById(id).isPresent()) {
-            authorService.deleteAuthor(id);
+            authorService.deleteAuthor(id); 
             return ResponseEntity.noContent().build();
         } else {
             return ResponseEntity.notFound().build();
